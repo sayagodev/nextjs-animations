cae6fd7aea4fad2fec55c690d6b0ab5b95799d21
Ángel Sáyago <sayagoadd@gmail.com>
2026-06-29 20:16:53 -0600

    feat: add grainient

--- page-reveal-sayago/src/app/page.tsx
+++ page-reveal-sayago/src/app/page.tsx
@@ -2,6 +2,16 @@
 
 import { UsePageReveal } from "@/hooks/use-page-reveal";
 import { useRef } from "react";
+import Link from "next/link";
+import Grainient from "@/components/effects/grainient";
+
+const gradients = [
+  { color1: "#BADFDB", color2: "#FFBDBD", color3: "#FCF9EA" },
+  { color1: "#210F37", color2: "#4F1C51", color3: "#A55B4B" },
+  { color1: "#FF97D0", color2: "#B331F1", color3: "#FBF5A7" },
+  { color1: "#BADFDB", color2: "#FF97D0", color3: "#FCF9EA" },
+  { color1: "#210F37", color2: "#4F1C51", color3: "#FF97D0" },
+];
 
 export default function Home() {
   const containerRef = useRef<HTMLDivElement>(null)
@@ -23,8 +33,34 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <section>
-        <h1>hola</h1>
+
+      <nav>
+        <div className="nav-logo">
+          <Link href="#">Foundry & Form<br /> Industrial Design Consultancy</Link>
+        </div>
+        <div className="nav-items">
+          <Link href="#">Work</Link>
+          <Link href="#">Catalogue</Link>
+          <Link href="#">About</Link>
+        </div>
+      </nav>
+
+      <section className="hero">
+        {gradients.map((g, i) => (
+          <div key={i} className={`intro-img ${i === 2 ? 'hero-img' : ''}`}>
+            <Grainient color1={g.color1} color2={g.color2} color3={g.color3} />
+          </div>
+        ))}
+        <div className="hero-content">
+          <div className="hero-header">
+            <h1>Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.</h1>
+          </div>
+          <div className="hero-social">
+            <p>Say Hello</p>
+            <Link href="#">info@foundryandform.com</Link>
+            <Link href="#">View Enquirires</Link>
+          </div>
+        </div>
       </section>
     </div>
   );
